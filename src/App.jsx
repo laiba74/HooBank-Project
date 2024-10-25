a75f1e44d8e855b15a47b2fbd392073e107516d3
laiba waseem <laibawasim74@gmail.com>
2024-10-26 00:28:28 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+// import React from "react";
+import style from "./style";
+
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  Cta,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${style.paddingX} ${style.flexCenter}`}>
+        <div className={`${style.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.flexStart}`}>
+        <div className={`${style.boxWidth} `}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+        <div className={`${style.boxWidth} `}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <Cta />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
